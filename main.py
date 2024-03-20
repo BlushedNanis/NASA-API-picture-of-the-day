b907d7b0b60afb3d8358680eb3f600e16495a0fb
BlushedNanis <luis_danieltg@outlook.com>
2024-03-19 20:25:02 -0400

    Added body to the web page

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ response = get("https://api.nasa.gov/planetary/"
 image = get(response["hdurl"]).content
 with open("image.jpg", "wb") as file:
     file.write(image)
+    
+#Set web body
+st.title(response["title"])
+
+#You can directly give the image url and forget about downloading the image,
+#but for learning purposed I did it this way.
+st.image("Image.jpg", (f"{response["copyright"].strip("\n")}"
+         f" / {response["date"]}"))
+
+st.markdown(f"<div style='text-align: justify;'>{response["explanation"]}</div>",
+            True)
